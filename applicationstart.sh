2fe4e631e482b5508a4667fc571e208e9367847e
neerajallen <100480612+neerajallen@users.noreply.github.com>
2023-02-16 11:28:05 +0530

    Update applicationstart.sh

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 cd /home/ubuntu
 pm2 -f start server.js
